a1c6e778b6569ec3e1ca569e0467a0a32972bce6
Olep8h <panov@student.vspj.cz>
2023-07-19 14:33:36 +0200

    import and stylization in App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import CurrencyConverter from './CurrencyConverter';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+            <div className="w-full max-w-md p-6 bg-white shadow-md rounded-lg">
+                <h1 className="text-3xl font-semibold mb-4 text-center text-blue-500">
+                    Currency Converter
+                </h1>
+                <CurrencyConverter />
+            </div>
+        </div>
+    );
 }
 
 export default App;
